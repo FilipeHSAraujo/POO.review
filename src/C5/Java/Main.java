c3af50fc5714b37da43576930a5291e81ba083b3
FilipeHSAraujo <filipehsaraujo@gmail.com>
2026-04-05 00:07:32 -0300

    Mudanças

--- src/C5/Java/Main.java
+++ src/C5/Java/Main.java
@@ -1,14 +1,48 @@
 package C5.Java;
 
+// 1. Classe base para todos os Ninjas
+abstract class Ninja {
+    protected String nome;
+
+    public Ninja(String nome) {
+        this.nome = nome;
+    }
+
+    // Método abstrato: cada clã DEVE implementar o seu
+    public abstract void habilidadeEspecial();
+}
+
+// 2. Especialização do Clã Uzumaki
+class Uzumaki extends Ninja {
+    public Uzumaki(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void habilidadeEspecial() {
+        System.out.println(nome + " usou: Rasengan!");
+    }
+}
+
+// 3. Especialização do Clã Uchiha
+class Uchiha extends Ninja {
+    public Uchiha(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void habilidadeEspecial() {
+        System.out.println(nome + " usou: Chidori!");
+    }
+}
+
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        // Uso de Polimorfismo: tratamos todos como "Ninja"
+        Ninja naruto = new Uzumaki("Naruto Uzumaki");
+        Ninja sasuke = new Uchiha("Sasuke Uchiha");
 
-        Uzumaki Naruto = new Uzumaki();
-        Naruto.nome = "Naruto Uzumaki";
-        Naruto.HabilidadeEspecial();
-
-        Uchiha Sasuke = new Uchiha();
-        Sasuke.nome = "Sasuke Uchiha";
-        Sasuke.HabilidadeEspecial();
+        naruto.habilidadeEspecial();
+        sasuke.habilidadeEspecial();
     }
 }
